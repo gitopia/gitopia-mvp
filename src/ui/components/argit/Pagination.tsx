a67047f5f0b42edd71a064c01101b840e81aa054
Snehil Buxy <snehil@mudrex.com>
2021-03-05 13:02:05 +0530

    Removed unused imports from Layout, Pagination. Added index in repo list to fix react warning

--- src/ui/components/argit/Pagination.tsx
+++ src/ui/components/argit/Pagination.tsx
@@ -5,7 +5,6 @@ import {
 } from "reactstrap"
 import * as React from "react"
 import { Link } from "react-router-dom"
-import { setTxLoading } from "../../reducers/argit"
 
 export class Pagination extends React.Component {
   constructor(props) {
@@ -125,47 +124,45 @@ export class Pagination extends React.Component {
             <p>The user has no repositories.</p>
           ))}
         {!this.props.txLoading &&
-          this.state.currentObjs.map(obj => {
+          this.state.currentObjs.map((obj, index) => {
             return (
-              <>
-                <ul>
-                  <li key={this.props.mainItems.repos[obj].txid}>
-                    {" "}
-                    <div>
-                      {this.props.mainItems.repos[obj].name && (
-                        <Link
-                          to={`/${this.props.match.params.wallet_address}/${
-                            this.props.mainItems.repos[obj].name
-                          }`}
-                          onClick={() => {
-                            this.props.setTxLoading({ loading: true })
-                            this.props.updatePage({ page: "repo" })
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            width="27"
-                            height="27"
-                            className="repo-list"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
-                            />
-                          </svg>
-                          <span>{this.props.mainItems.repos[obj].name}</span>
-                        </Link>
-                      )}
-                    </div>
+              <ul key={index}>
+                <li key={this.props.mainItems.repos[obj].txid}>
+                  {" "}
+                  <div>
                     {this.props.mainItems.repos[obj].name && (
-                      <button>
-                        <i className="fa fa-check-circle" />
-                      </button>
+                      <Link
+                        to={`/${this.props.match.params.wallet_address}/${
+                          this.props.mainItems.repos[obj].name
+                        }`}
+                        onClick={() => {
+                          this.props.setTxLoading({ loading: true })
+                          this.props.updatePage({ page: "repo" })
+                        }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 16 16"
+                          width="27"
+                          height="27"
+                          className="repo-list"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+                          />
+                        </svg>
+                        <span>{this.props.mainItems.repos[obj].name}</span>
+                      </Link>
                     )}
-                  </li>
-                </ul>
-              </>
+                  </div>
+                  {this.props.mainItems.repos[obj].name && (
+                    <button>
+                      <i className="fa fa-check-circle" />
+                    </button>
+                  )}
+                </li>
+              </ul>
             )
           })}
         {!this.props.txLoading &&
